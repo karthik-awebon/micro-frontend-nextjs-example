86f6e30a1a3fdeafc96e00f17eb19064e0082eb4
Karthikeyan Balasubramanian <jkarthi.online@gmail.com>
2025-03-12 21:25:06 +0530

    params fix

--- blog/app/blog/post/[id]/page.tsx
+++ blog/app/blog/post/[id]/page.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-export default function Post({ params: { id } }) {
+export default function Post({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
       <h3>Post #{id}</h3>
       <p>Lorem ipsum</p>
-      <Link href="/blog">Back to blog</Link>
+      <Link href='/blog'>Back to blog</Link>
     </div>
   );
 }
